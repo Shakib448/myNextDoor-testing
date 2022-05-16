9bf9ef25b061c7fc038db3060567893b2458f381
Shakiba448 <shakiba448@gmail.com>
2022-05-16 11:36:06 +0600

    WIP on server

--- server/utils/server.ts
+++ server/utils/server.ts
@@ -3,11 +3,16 @@ import { connectDB } from "@config/db";
 import express, { Express } from "express";
 import addValueRoutes from "@routes/AddValue.router";
 import cors from "cors";
+import compression from "compression";
 
 const app: Express = express();
 
+// middleware
 app.use(express.json());
 app.use(cors());
+app.use(compression());
+
+// routes
 app.use("/api/add-value", addValueRoutes);
 
 connectDB().catch((err) => console.log(err));
