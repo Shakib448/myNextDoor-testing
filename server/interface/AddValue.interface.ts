e916b0a19c30434c417ef4033899d047a08a75fd
Shakiba448 <shakiba448@gmail.com>
2022-05-16 06:48:58 +0600

    WIP on server

---
+++ server/interface/AddValue.interface.ts
@@ -0,0 +1,3 @@
+export interface ADD_VALUE {
+  name: string;
+}
---
+++ server/routes/AddValue.router.ts
@@ -0,0 +1,7 @@
+import express from "express";
+
+const router = express.Router();
+
+router.route("/add-value").post();
+
+export default router;
