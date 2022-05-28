2b939f0f3b8d80506e732bd9e81cdb351597caf3
Muktadir Ahamed Shakib <muktadirahamedshakib@Muktadirs-MacBook-Pro.local>
2022-05-28 12:33:42 +0600

    WIP on myNextdoor

--- server/main.ts
+++ server/main.ts
@@ -5,6 +5,6 @@ const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, (): void =>
   console.log(
-    `GraphQL is now running on http://localhost:${PORT}`.magenta.italic
+    `GraphQL is now running on mac http://localhost:${PORT}`.magenta.italic
   )
 );
