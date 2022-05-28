3de769b8e7ffe9201a66ea18166c7aa5fbf836ea
Shakib448 <shakiba448@gmail.com>
2022-05-28 18:20:39 +0600

    FIX: button issue

--- client/next.config.js
+++ client/next.config.js
@@ -3,20 +3,7 @@ const { i18n } = require("./next-i18next.config");
 const nextConfig = {
   reactStrictMode: true,
   i18n,
-  async rewrites() {
-    return [
-      {
-        source: "/",
-        destination: "/",
-        locale: false,
-      },
-      {
-        source: "/fr",
-        destination: "/fr",
-        locale: false,
-      },
-    ];
-  },
+  swcMinify: false,
 };
 
 module.exports = nextConfig;
--- client/src/components/Login.tsx
+++ client/src/components/Login.tsx
@@ -81,7 +81,7 @@ const Login = () => {
       </button>
 
       <div className="flex flex-wrap -mx-3 mb-2">
-        <div className="w-full md:w-1/3 px-3 mb-6 md:mb-0">
+        <div className="w-full px-3 mb-6 md:mb-0">
           <label
             className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
             htmlFor="grid-state"
